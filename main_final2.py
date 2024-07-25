7ce21ceb64ed40b58ddec2ca1d828ca0f392ee04
ravi6389 <77967899+ravi6389@users.noreply.github.com>
2024-07-25 23:23:21 +0530

    Update main_final2.py

--- main_final2.py
+++ main_final2.py
@@ -106,8 +106,7 @@ def run_llm(query: str, chat_history: List[Dict[str, Any]] = []):
     else:
         embeddings = st.session_state['embeddings']
     if(st.session_state['db'] ==''):
-        st.write(st.session_state['split_1'])
-        # st.write(embeddings)
+        
         db = FAISS.from_documents(st.session_state['split_1'], embeddings)
         st.session_state['db'] = db
     else:
