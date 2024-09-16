545a9fe03d2baf8f92c9a32614d4c61dc6b4a86d
ravi6389 <77967899+ravi6389@users.noreply.github.com>
2024-09-16 19:00:08 +0530

    Update main_final2.py

--- main_final2.py
+++ main_final2.py
@@ -138,15 +138,15 @@ def run_llm(query: str, chat_history: List[Dict[str, Any]] = []):
 
     rephrase_prompt = hub.pull("langchain-ai/chat-langchain-rephrase")
     
-    retrieval_qa_chat_prompt = hub.pull("langchain-ai/retrieval-qa-chat")
+    #retrieval_qa_chat_prompt = hub.pull("langchain-ai/retrieval-qa-chat")
 
-    # retrieval_qa_chat_prompt = "Answer any use questions based solely on the context below:\
+    retrieval_qa_chat_prompt = """Answer any use questions based solely on the context below:\
 
-    # <context>\
+    <context>\
     
-    # {context}\
+    {context}\
     
-    # </context>"
+    </context>"""
     
     stuff_documents_chain = create_stuff_documents_chain(llm, retrieval_qa_chat_prompt)
 
