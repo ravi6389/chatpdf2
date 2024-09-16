e99656694697830751536ae672c12472ec7a1335
ravi6389 <77967899+ravi6389@users.noreply.github.com>
2024-09-16 18:56:30 +0530

    Update main_final2.py

--- main_final2.py
+++ main_final2.py
@@ -140,11 +140,11 @@ def run_llm(query: str, chat_history: List[Dict[str, Any]] = []):
     
     #retrieval_qa_chat_prompt = hub.pull("langchain-ai/retrieval-qa-chat")
 
-    retrieval_qa_chat_prompt = 'Answer any use questions based solely on the context below:
+    retrieval_qa_chat_prompt = 'Answer any use questions based solely on the context below:\
 
-    <context>
+    <context>\
     
-    {context}
+    {context}\
     
     </context>'
     
