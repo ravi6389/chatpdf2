f301c90a3430ffffdcae04aafa22b66213629f9d
ravi6389 <77967899+ravi6389@users.noreply.github.com>
2024-09-16 18:44:43 +0530

    Update main_final2.py

--- main_final2.py
+++ main_final2.py
@@ -144,7 +144,7 @@ def run_llm(query: str, chat_history: List[Dict[str, Any]] = []):
 {context}
 
 </context>
-If you find a relevant hyperlink, please include it
+# If you find a relevant hyperlink, please include it
 If you don't find the context, please respond \'I do not know\'"
     retrieval_qa_chat_prompt = hub.pull("langchain-ai/retrieval-qa-chat")
     stuff_documents_chain = create_stuff_documents_chain(llm, retrieval_qa_chat_prompt)
