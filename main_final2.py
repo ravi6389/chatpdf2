b665a3af057ee49397a4bfbb69d6f4ad13f43bd6
ravi6389 <77967899+ravi6389@users.noreply.github.com>
2024-09-17 11:25:04 +0530

    Update main_final2.py

--- main_final2.py
+++ main_final2.py
@@ -141,7 +141,7 @@ def run_llm(query: str, chat_history: List[Dict[str, Any]] = []):
 
     retrieval_qa_chat_prompt = PromptTemplate.from_template(
     """
-   Answer any use questions based solely on the context below. If you do not find the context, say context does not provide the answer even if you know the answer:
+   Answer any use questions based solely on the context below. Do not use your own knowledge to answer the questions:
 
 <context>
 
