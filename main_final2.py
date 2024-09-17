c43cf10141db8d757e4fcb26a1e67ef80652cf90
ravi6389 <77967899+ravi6389@users.noreply.github.com>
2024-09-17 11:42:22 +0530

    Update main_final2.py

--- main_final2.py
+++ main_final2.py
@@ -143,7 +143,7 @@ def run_llm(query: str, chat_history: List[Dict[str, Any]] = []):
     """
     Use the following pieces of context to answer the question at the end. If you 
     don't know the answer, just say that you don't know, don't try to make up an 
-    answer.
+    answer. If you find hyperlink in the context, inlcude that in answer.
 
     {context}
 
