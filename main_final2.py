f34a11cf79e2db4aaad8ca89655882cc640e7c27
ravi6389 <77967899+ravi6389@users.noreply.github.com>
2024-11-21 10:38:19 +0530

    Update main_final2.py

--- main_final2.py
+++ main_final2.py
@@ -73,7 +73,7 @@ if (st.session_state['run_once'] == 0):
     # os.environ['CURL_CA_BUNDLE'] = ''
     # if (st.session_state['run_once'] == 0):
     #reader = PdfReader('C:\\Users\\RSPRASAD\\OneDrive - Danaher\\Learning\\Hackathon\\BecLS_Website_v2.pdf')
-    reader = PdfReader('website.pdf')
+    reader = PdfReader('beckman_website.pdf')
     
     for page in reader.pages:
         documents_1 += page.extract_text()
@@ -115,7 +115,7 @@ def run_llm(query: str, chat_history: List[Dict[str, Any]] = []):
     # db.save_local('vector store\\becki2')
     if(st.session_state['loaded_db'] == ''):
     
-        loaded_db = FAISS.load_local('vector store/becki',\
+        loaded_db = FAISS.load_local('vector store2/becki',\
     embeddings, allow_dangerous_deserialization=True)
         st.session_state['loaded_db'] = loaded_db
     
